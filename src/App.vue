5864f6cafa51ad5d443aa2aef1bf57d7f66fe7ff
jasonvandewarker <jasonvandewarker@gmail.com>
2017-10-18 11:51:15 -0700

    Added the create todo stuff

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
   <div id="app">
     <todo-list v-bind:todos="todos"></todo-list>
+    <create-todo v-on:add-toto="addTodo"></create-todo>
   </div>
 </template>
 
 <script>
 import TodoList from './components/TodoList';
-
+import CreateTodo from './components/CreateTodo';
 
 export default {
   name: 'app',
   components: {
     TodoList,
+    CreateTodo,
+  },
+
+  methods: {
+    addTodo(title) {
+      this.todos.push({
+        title,
+        done: false,
+      });
+    },
   },
 
   data() {
